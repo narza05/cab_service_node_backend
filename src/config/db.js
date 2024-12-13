0108cbf0a84a5e3b110b79e7c44b187e133c928a
narza05 <ankushsaini05112002@gmail.com>
2024-12-13 21:04:12 +0530

    database hosting update

--- src/config/db.js
+++ src/config/db.js
@@ -10,6 +10,11 @@ const pool = new Pool({
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
     port: process.env.DBPORT,
+    idleTimeoutMillis: 30000, // Close idle connections after 30 seconds
+    connectionTimeoutMillis: 5000,
+    ssl: {
+        rejectUnauthorized: false, // Use true if you have a valid certificate
+    },
 })
 
 pool.on("connect",()=>{
